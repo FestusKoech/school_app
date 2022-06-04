d36d59e0641093c7bc9a745bd9f53a2936a76dc0
FlutLab.io <>
2022-06-04 10:11:40 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,75 +1,291 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
-
+class LoginScreen extends StatefulWidget {
   @override
-  State<Login> createState() => _LoginPageState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginPageState extends State<Login> {
+class _LoginScreenState extends State<LoginScreen> {
+  bool rememberpwd = false;
+  bool sec = true;
+  var visable = Icon(
+    Icons.visibility,
+    color: Color(0xff4c5166),
+  );
+  var visableoff = Icon(
+    Icons.visibility_off,
+    color: Color(0xff4c5166),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.orange[50],
-        body: Column(children: [
-          SizedBox(height: 30),
-          //username
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: Colors.orange[400],
-            margin: const EdgeInsets.all(25),
-            padding: const EdgeInsets.all(25),
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5.0),
-              child: Text('Africa Nazarene University', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32, color: Colors.red)),
-            ),
-          ),
-
-          SizedBox(height: 15),
-
-          Text('Login Page', style: TextStyle(fontSize: 25, color: Colors.black)),
-
-          SizedBox(height: 15),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              //password
-              child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(12),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xff65b0bb),
+                      Color(0xff5a9ea8),
+                      Color(0xff508c95),
+                      Color(0xff467b82),
+                      Color(0xff3c6970),
+                      Color(0xff32585d),
+                      Color(0xff28464a),
+                    ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 25.0),
-                    child: TextField(decoration: InputDecoration(border: InputBorder.none, hintText: 'Username')),
-                  ))),
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            //password
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 100,
+                        ),
+                        Text(
+                          "Hello User! ☺️",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                        ),
+                        buildEmail(),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        buildPassword(),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            buildRememberassword(),
+                            buildForgetPassword()
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        buildLoginButton(),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            buildFacebook(),
+                            buildGoogle(),
+                            buildTwitter()
+                          ],
+                        ),
+                        SizedBox(
+                          height: 60,
+                        ),
+                        Text(
+                          "الشروط والاحكام",
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 25.0),
-                child: TextField(decoration: InputDecoration(border: InputBorder.none, hintText: 'Password')),
-              ),
-            ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
 
-          //Remember Username
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Email",
+          style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(color: Color(0xffebefff), borderRadius: BorderRadius.circular(10), boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+            )
+          ]),
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Color(0xff4c5166),
+                ),
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.black38)),
+          ),
+        ),
+      ],
+    );
+  }
 
-          //login button
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Password",
+          style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Color(0xffebefff),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+            ],
+          ),
+          height: 60,
+          child: TextField(
+            obscureText: sec,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      sec = !sec;
+                    });
+                  },
+                  icon: sec ? visableoff : visable,
+                ),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.vpn_key,
+                  color: Color(0xff4c5166),
+                ),
+                hintText: "pwd",
+                hintStyle: TextStyle(color: Colors.black38)),
+          ),
+        )
+      ],
+    );
+  }
 
-          //forgot password
+  Widget buildRememberassword() {
+    return Container(
+      height: 20,
+      child: Row(
+        children: [
+          Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.white),
+              child: Checkbox(
+                value: rememberpwd,
+                checkColor: Colors.blueGrey,
+                activeColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    rememberpwd = true;
+                  });
+                },
+              )),
+          Text(
+            "Remember me",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
 
-          //footer
-        ]));
+  Widget buildForgetPassword() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        child: Text("Forget Password !", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget buildLoginButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      child: Container(
+        width: double.infinity,
+        child: RaisedButton(
+          onPressed: () {},
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          color: Color(0xff3c6970),
+          padding: EdgeInsets.all(30),
+          child: Text(
+            "Login",
+            style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildFacebook() {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Image.asset("assets/facebook.png"),
+    );
+  }
+
+  Widget buildGoogle() {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Image.asset("assets/search.png"),
+    );
+  }
+
+  Widget buildTwitter() {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Image.asset("assets/twitter.png"),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'loginPage.dart';
 
-void main() => runApp(MyApp());
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: LoginScreen(),
     );
   }
 }
